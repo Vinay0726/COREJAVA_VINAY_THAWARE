b20bf10cd9a2ea11668e17574b5c3ada9ea8691f
Vinay0726 <vinaythaware143@gmail.com>
2024-01-08 11:35:11 +0530

    new java file added

--- src/com/mkpits/arrays/MultidimentionalArray.java
+++ src/com/mkpits/arrays/MultidimentionalArray.java
@@ -5,8 +5,7 @@ public class MultidimentionalArray {
 	public static void main(String[] args) {
 		//multi-dimentional an array elements are referred using multiple indexed(matrix)
 		//It increase complexity
-		String[][]name={ {"mr.","ms."},{"vinay","raj","sam"}};
-		
+		String[][]name={ {"mr.","ms."},{"vinay","raj","sam"}};   
 		System.out.println("Name :-"+name[0][0]+name[1][0]);
         System.out.println("Name :-"+name[0][0]+name[1][1]);
         System.out.println("Name :-"+name[0][1]+name[1][2]);
--- src/com/mkpits/cfs/SnakeGunWater.java
+++ src/com/mkpits/cfs/SnakeGunWater.java
@@ -12,7 +12,12 @@ public class SnakeGunWater {
 		
 		System.out.println("------------------");
 		System.out.println("Welcome Snake Water Game");
-		int random=(int)Math.floor(Math.random()*100);
+		char ch='Y';
+		//using while loop fetch old data
+		while(ch=='Y'|| ch=='y') {
+	    System.out.println("Enter 1-32=s \n 33-65=w \n 66-99=g");
+		int random=Integer.parseInt(br.readLine());
+		random=Integer.parseInt(br.readLine());
 		
 		if(random>1 && random<33) {
 			sysInput="s";
@@ -23,26 +28,18 @@ public class SnakeGunWater {
 		if(random>66 && random<100) {
 			sysInput="g";
 		}
-		char ch='Y';
-		//using while loop fetch old data
-		while(ch=='Y'|| ch=='y') {
-		System.out.println("Enter your choice:\n1)s \n2)w \n3)g");
-		String userInput=br.readLine();
-	 
-	    if((sysInput.equalsIgnoreCase("S")&& userInput.equalsIgnoreCase("G") || sysInput.equalsIgnoreCase("W")&&userInput.equalsIgnoreCase("S"))) {
+		
+		
+	    if((sysInput.equalsIgnoreCase("w"))) {
 	    System.out.println("Snake win ");
 	    }
-		
-	    if((sysInput.equalsIgnoreCase("W")&& userInput.equalsIgnoreCase("S") || sysInput.equalsIgnoreCase("G")&& userInput.equalsIgnoreCase("W"))) {
-	    System.out.println("Water win ");
-	    }
-		
-	    if((sysInput.equalsIgnoreCase("S")&& userInput.equalsIgnoreCase("G") || sysInput.equalsIgnoreCase("G")&& userInput.equalsIgnoreCase("S"))) {
-	    System.out.println("Gun win ");
-	    }
+	    
+	    
 	    System.out.println("Do you wish to continue y/n");
 	    ch=(char)br.read();
 	    br.readLine();
+	    
 	}
- }		
+	}
 }
+
