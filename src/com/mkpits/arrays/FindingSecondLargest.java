20c6aac0c1578f16cbf1d83a0aebf92c67b238d2
Vinay0726 <vinaythaware143@gmail.com>
2024-01-09 12:37:13 +0530

    Finding second largest number

---
+++ src/com/mkpits/arrays/FindingSecondLargest.java
@@ -0,0 +1,17 @@
+package com.mkpits.arrays;
+
+import java.util.Arrays;
+
+public class FindingSecondLargest {
+
+	public static void main(String[] args) {
+		//For Second Highest number using Arrays.sort
+		int[] num= {1 ,9 ,8, 10, 3 ,6};
+		Arrays.sort(num);
+		for (int index = 0; index < num.length; index++) {
+			System.out.println(num[index]);
+		}
+		
+		System.out.println("Last highest value: " + num[num.length - 2]);
+		}
+}
